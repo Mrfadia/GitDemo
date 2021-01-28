b1e60f60d50b31f948e56aff5b6bc3a35703536c
Mrfadia <fadiaridham2@gmail.com>
2021-01-28 16:30:31 -0500

    first commit

---
+++ src/test/java/qaclickacademy/Section22Mavenjava/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Section22Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void NativeApAndroid() {
+		
+		System.out.println("NativeApAndroid");
+	}
+	
+	
+	@Test
+	public void IOSApps() {
+		
+		System.out.println("IOSApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/Section22Mavenjava/RestApiTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Section22Mavenjava;
+
+import org.testng.annotations.Test;
+
+
+public class RestApiTest {
+
+	@Test
+	public void postjira() {
+		System.out.println("postjira");
+	}
+	
+	
+	@Test
+	public void deletTwitter() {
+		
+		System.out.println("deletTwitter");
+	}
+	
+}
